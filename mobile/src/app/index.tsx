2c146b29e9ae1c7ccd70fb58204149503d58e2aa
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-12-15 19:10:15 -0300

    feat: implementação do botão começar

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,14 +1,19 @@
-import { View, Text } from "react-native";
+import { View } from "react-native"
+import { router } from "expo-router"
 
-import { Welcome } from "@/components/welcome";
-import { Steps } from "@/components/steps";
+import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
+import { Welcome } from "@/components/welcome"
 
 export default function Index() {
-    return (
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
 
-        <View style={{ flex: 1, padding: 40, gap: 40 }} >
-            <Welcome />
-            <Steps />
-        </View>
-    )
+      <Button onPress={() => router.navigate("/home")}>
+        <Button.Title>Começar</Button.Title>
+      </Button>
+    </View>
+  )
 }
