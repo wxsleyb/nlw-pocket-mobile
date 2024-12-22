3e9837d2c347ddebba1c8c9e643c62d931f3e1e3
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-12-22 20:07:35 -0300

    feat: imagem, styles, icone, nome e descrição dos lugares

---
+++ mobile/src/components/place/index.tsx
@@ -0,0 +1,38 @@
+import { IconTicket } from "@tabler/icons-react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps, View, Image } from "react-native";
+
+import {s} from "./styles"
+
+import {colors, fontFamily} from "@/styles/theme"
+
+export type PlaceProps = {
+    id: string
+    name: string
+    description: string
+    coupons: number
+    cover: string
+    address: string
+}
+
+type Props = TouchableOpacityProps & {
+    data: PlaceProps
+}
+
+export function Place({data, ...rest}: Props){
+    return (
+        <TouchableOpacity style={s.container} {...rest}>
+            <Image style={s.image} source={{uri: data.cover}}/>
+
+            <View style={s.content}>
+                <Text style={s.name}>{data.name}</Text>
+                <Text style={s.description}>{data.description}</Text>
+
+                <View style={s.footer}>
+                    <IconTicket size={16} color={colors.red.base}/>
+                    <Text style={s.tickets}> {data.coupons} cupons disponíveis</Text>
+                </View>
+
+            </View>
+        </TouchableOpacity>
+    )
+}
