f739b4f2b521d64502c8475489b41c89ed8c775e
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-12-22 20:06:02 -0300

    fix: problemas na home

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,33 +1,61 @@
-import { Alert, Text, View } from "react-native";
+import { useEffect, useState } from "react"
+import { View, Alert } from "react-native"
 
-import { useEffect, useState } from "react";
+import { api } from "@/services/api"
 
-import { api } from "@/services/api";
-import { Categories, CategoriesProps } from "@/components/categories";
+import { Places } from "@/components/places"
+import { PlaceProps } from "@/components/place"
+import { Categories, CategoriesProps } from "@/components/categories"
+
+type MarketsProps = PlaceProps
 
 export default function Home() {
-    const [categories, setCategories] = useState<CategoriesProps>([])
-    const [category, setCategory] = useState("")
+  const [categories, setCategories] = useState<CategoriesProps>([])
+  const [category, setCategory] = useState("")
+  const [markets, setMarkets] = useState<MarketsProps[]>([])
 
-    async function fetchCategories() {
-        try {
-            const { data } = await api.get("/categories")
-            setCategories(data)
-            setCategory(data[0].id)
-        } catch (error) {
-            console.log(error)
-            Alert.alert("Categorias", "Não foi possível carregar as categorias.")
-        }
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories")
+      setCategories(data)
+      setCategory(data[0].id)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.")
     }
+  }
 
-    useEffect(() => {
-        fetchCategories()
-    }, [])
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return
+      }
 
+      const { data } = await api.get("/markets/category/" + category)
+      setMarkets(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Locais", "Não foi possível carregar os locais.")
+    }
+  }
 
-    return (
-        <View style={{ flex: 1 }}>
-            <Categories data={categories} />
-        </View>
-    )
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  useEffect(() => {
+    fetchMarkets()
+  }, [category])
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+      <Categories
+        data={categories}
+        onSelect={setCategory}
+        selected={category}
+      />
+
+      <Places data={markets} />
+    </View>
+  )
 }
