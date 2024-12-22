39d53c8c6354c18e2ea992ce322670fee9476b79
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-12-22 20:07:42 -0300

    fix: baseURL api

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: "http://192.168.1.111:3333",
   timeout: 700,
   withCredentials: true, // Adicione isso caso precise enviar cookies ou autenticação
 
