90ba3fdc530b9f73c9c14373a92ba2980d58c063
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-12-22 20:07:02 -0300

    feat: categoria e icones

---
+++ mobile/src/components/category/index.tsx
@@ -0,0 +1,27 @@
+
+import { Text, Pressable, PressableProps } from "react-native"
+
+import { s } from "./styles"
+import { categoriesIcons } from "@/utils/categories-icons"
+
+import { colors } from "@/styles/theme"
+type Props = PressableProps & {
+    iconId: string
+    isSelected?: boolean
+    name: string
+}
+
+export function Category({ name, iconId, isSelected = false, ...rest }: Props) {
+
+    const Icon = categoriesIcons[iconId]
+
+
+    return (
+        <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest} >
+
+            <Icon size={16} color={colors.gray[isSelected ? 100 : 400]}></Icon>
+            <Text style={[s.name, isSelected && s.nameSelected]} > {name} </Text>
+
+        </Pressable>
+    )
+}
